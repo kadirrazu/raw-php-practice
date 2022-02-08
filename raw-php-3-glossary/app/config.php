48f37057b053a8ec2748bc3fe255fc92df2b1bef
unknown <razu121@gmail.com>
2022-02-08 07:35:12 +0600

    Added admin folder in root and views

--- raw-php-3-glossary/app/config.php
+++ raw-php-3-glossary/app/config.php
@@ -1,7 +1,7 @@
 <?php
 
 const CONFIG = [
-    "data_file" => "data.json",
+    "data_file" => APP_PATH . "data.json",
     "users" => [
         "admin@url.com" => "123456",
         "kadirrazu@github.com" => "123456"
